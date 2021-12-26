65ea0791f118b8ff3461bb575d2df4cdaad02b02
Chris <larocque.christopher@gmail.com>
2021-12-26 17:15:54 -0500

    lint

--- components/UserAuthForm.vue
+++ components/UserAuthForm.vue
@@ -1,30 +1,62 @@
 <template>
   <div class="w-full max-w-sm">
-    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" v-model="valid">
-      <div class="mb-4" v-if="hasName">
+    <form
+      v-model="valid"
+      class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+    >
+      <div v-if="hasName" class="mb-4">
         <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
           Name
         </label>
-        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" v-model="userInfo.name">
+        <input
+          id="username"
+          v-model="userInfo.name"
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          type="text"
+          placeholder="Username"
+        />
       </div>
       <div class="mb-4">
         <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
           Email
         </label>
-        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" placeholder="Email" v-model="userInfo.email">
+        <input
+          id="email"
+          v-model="userInfo.email"
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          type="text"
+          placeholder="Email"
+        />
       </div>
       <div class="mb-6">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+        <label
+          class="block text-gray-700 text-sm font-bold mb-2"
+          for="password"
+        >
           Password
         </label>
-        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" v-model="userInfo.password" >
+        <input
+          id="password"
+          v-model="userInfo.password"
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          type="password"
+          placeholder="******************"
+        />
         <!-- <p class="text-red-500 text-xs italic" v-if="notValid">Please choose a password.</p> -->
       </div>
       <div class="flex items-center justify-between">
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" v-on:click="submitForm(userInfo)" :disabled="!valid">
+        <button
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="button"
+          :disabled="!valid"
+          @click="submitForm(userInfo)"
+        >
           {{ buttonText }}
         </button>
-        <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+        <a
+          class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+          href="#"
+        >
           Forgot Password?
         </a>
       </div>
@@ -33,17 +65,17 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        valid: true,
-        showPassword: false,
-        userInfo: {
-          email: 'test@test.com',
-          password: '123123'
-        },
-      }
-    },
-    props: ["submitForm", "buttonText", "hasName"]
-  }
+export default {
+  props: ["submitForm", "buttonText", "hasName"],
+  data() {
+    return {
+      valid: true,
+      showPassword: false,
+      userInfo: {
+        email: "test@test.com",
+        password: "123123",
+      },
+    };
+  },
+};
 </script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,37 +1,40 @@
-const config = require('./.contentful.json');
+const config = require("./.contentful.json");
 
 export default {
-  target: 'static', // default is 'server'
-	modules: ['@nuxt/http', '@nuxtjs/markdownit', '@nuxtjs/axios', '@nuxtjs/auth-next'],
-  buildModules: ['@nuxtjs/tailwindcss'],
-  plugins: ['~/plugins/preview.client.js'],
-	head: {
-    title: 'Wifeapedia',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: 'Fan wiki for reality show WifeSwap'
-      }
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
-  },
-	env: {
-		CTF_SPACE_ID: config.CTF_SPACE_ID,
-    CTF_CDA_ACCESS_TOKEN: config.CTF_CDA_ACCESS_TOKEN,
-    CTF_CPA_ACCESS_TOKEN: config.CTF_CPA_ACCESS_TOKEN
-	},
-  css: [
-    '~/assets/css/main.scss'
+  target: "static", // default is 'server'
+  modules: [
+    "@nuxt/http",
+    "@nuxtjs/markdownit",
+    "@nuxtjs/axios",
+    "@nuxtjs/auth-next",
   ],
+  buildModules: ["@nuxtjs/tailwindcss"],
+  plugins: ["~/plugins/preview.client.js"],
+  head: {
+    title: "Wifeapedia",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "Fan wiki for reality show WifeSwap",
+      },
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+  },
+  env: {
+    CTF_SPACE_ID: config.CTF_SPACE_ID,
+    CTF_CDA_ACCESS_TOKEN: config.CTF_CDA_ACCESS_TOKEN,
+    CTF_CPA_ACCESS_TOKEN: config.CTF_CPA_ACCESS_TOKEN,
+  },
+  css: ["~/assets/css/main.scss"],
   components: true,
   markdownit: {
     html: true,
   },
   generate: {
-    fallback: true
+    fallback: true,
   },
   // router: {
   //   middleware: ['auth']
@@ -39,35 +42,36 @@ export default {
   auth: {
     strategies: {
       social: {
-        scheme: 'oauth2',
+        scheme: "oauth2",
         endpoints: {
-          authorization: 'https://accounts.google.com/o/oauth2/auth',
+          authorization: "https://accounts.google.com/o/oauth2/auth",
           token: undefined,
-          userInfo: 'https://www.googleapis.com/oauth2/v3/userinfo',
-          logout: 'https://example.com/logout'
+          userInfo: "https://www.googleapis.com/oauth2/v3/userinfo",
+          logout: "https://example.com/logout",
         },
         token: {
-          property: 'access_token',
-          type: 'Bearer',
-          maxAge: 1800
+          property: "access_token",
+          type: "Bearer",
+          maxAge: 1800,
         },
         refreshToken: {
-          property: 'refresh_token',
-          maxAge: 60 * 60 * 24 * 30
+          property: "refresh_token",
+          maxAge: 60 * 60 * 24 * 30,
         },
-        responseType: 'code',
-        grantType: 'authorization_code',
-        accessType: 'offline',
-        redirectUri: 'http://localhost:3000/login',
-        logoutRedirectUri: 'http://localhost:3000/login',
-        clientId: '82783355399-ifi5bmi1tptg1hmpj9k98gq75rfucat7.apps.googleusercontent.com',
-        scope: ['openid', 'profile', 'email'],
-        state: 'UNIQUE_AND_NON_GUESSABLE',
-        codeChallengeMethod: '',
-        responseMode: '',
-        acrValues: '',
+        responseType: "code",
+        grantType: "authorization_code",
+        accessType: "offline",
+        redirectUri: "http://localhost:3000/login",
+        logoutRedirectUri: "http://localhost:3000/login",
+        clientId:
+          "82783355399-ifi5bmi1tptg1hmpj9k98gq75rfucat7.apps.googleusercontent.com",
+        scope: ["openid", "profile", "email"],
+        state: "UNIQUE_AND_NON_GUESSABLE",
+        codeChallengeMethod: "",
+        responseMode: "",
+        acrValues: "",
         // autoLogout: false
-      }
-    }
-  }
-}
+      },
+    },
+  },
+};
